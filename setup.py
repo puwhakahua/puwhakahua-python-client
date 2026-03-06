903745358a32c1a923180be70959e291719fd6dd
Peter Reutemann <fracpete@gmail.com>
2026-03-06 14:27:17 +1300

    initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_namespace_packages
+
+
+setup(
+    name="puwhakahua_api_client",
+    description="Python client for the Pūwhakahua API.",
+    url="https://github.com/puwhakahua/puwhakahua-api-client",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Multimedia :: Sound/Audio :: Speech',
+    ],
+    #license="GPL-3.0-only",
+    package_dir={
+        '': 'src'
+    },
+    packages=find_namespace_packages(where='src'),
+    install_requires=[
+        "setuptools",
+        "wheel",
+        "requests",
+    ],
+    version="0.0.1",
+    author='Peter Reutemann',
+    author_email='fracpete@waikato.ac.nz',
+)
